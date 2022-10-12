0eb0cf36432dea0adad88151949743ef24419a60
Cgristian-code <108216573+Cgristian-code@users.noreply.github.com>
2022-10-12 19:34:15 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Show some text
+
+function showText() {
+    document.getElementById("story").innerHTML = `
+    Jadi pertama kalinya saya cinta ngoding adalah
+    di saat pertamakali belajar HTML di pelajran
+    informatika. Nah saat itu baru pengenalan HTML
+    dasar. Nah karena saya tertarik jadi saya belajar
+    otodidak belajar sendiri lewat video youtube.
+    Pertamakali agar jadi expert aja di kelas, nah
+    tapi malah jadi keterusan. Dan akhirnya saya mendalami
+    front-end developer sampai sekarang. Kenapa lewat 
+    Youtube, karena gratis jadi saya tidak punya uang untuk
+    belajar online lewat course gitu. Makanya saya berterima kasih
+    kepada Id Camp yang menyediakan beasiswa gratis.
+    `
+};
